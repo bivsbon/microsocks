9a904b553840d933e589ceec794b8d662d01a415
bivsbon <thanhminh20002005@gmail.com>
2024-06-27 00:12:35 +0700

    comment out the new code

--- sockssrv.c
+++ sockssrv.c
@@ -409,9 +409,7 @@ static int handshake(struct thread *t) {
 				}
 				break;
 			case SS_3_AUTHED:
-				union sockaddr_union bind_addr = {.v4.sin_family = AF_UNSPEC};
-				ret = connect_socks_target(buf, n, &t->client, &bind_addr);
-				// dolog("Size of bind_addrs is: %d\n", sblist_getsize(bind_addrs));
+				dolog("Size of bind_addrs is: %d\n", sblist_getsize(bind_addrs));
 				// if (sblist_getsize(bind_addrs) == 0) {
 				// 	union sockaddr_union bind_addr = {.v4.sin_family = AF_UNSPEC};
 				// 	ret = connect_socks_target(buf, n, &t->client, &bind_addr);
@@ -420,6 +418,8 @@ static int handshake(struct thread *t) {
 				// 	dolog("Get at index: %d\n", index);
 				// 	ret = connect_socks_target(buf, n, &t->client, sblist_get(bind_addrs, index));
 				// }
+				union sockaddr_union bind_addr = {.v4.sin_family = AF_UNSPEC};
+				ret = connect_socks_target(buf, n, &t->client, &bind_addr);
 				if(ret < 0) {
 					send_error(t->client.fd, ret*-1);
 					return -1;
